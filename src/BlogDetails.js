897cfa6b8a3719e657f40cfcbeb9c6fa3afba89f
Davis SHYAKA <shyakakingdavis@gmail.com>
2022-07-05 18:34:21 +0200

    Fetch Fix

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -4,15 +4,11 @@ import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
-  const {
-    data: blog,
-    error,
-    isPending,
-  } = useFetch("http://localhost:8000/blogs/" + id);
+  const { data: blog, error, isPending } = useFetch("data/db.json/" + id);
   const history = useHistory();
 
   const handleClick = () => {
-    fetch("http://localhost:8000/blogs/" + blog.id, {
+    fetch("data/db.json/" + blog.id, {
       method: "DELETE",
     }).then(() => {
       history.push("/");
